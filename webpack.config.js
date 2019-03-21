79ed286b48f1ec078ad1acca7563466658bfda38
natureStory <915304053@qq.com>
2019-03-22 02:30:11 +0800

    配置webpack开发服务器和热更新以及代码分片

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+var webpack = require('webpack');
 module.exports = {
     mode: 'development',
     entry: "./src/index.tsx",
     output: {
+        publicPath:'/dist', // 必须加publicPath
+        chunkFilename: "static/js/[name].chunk.js",
         filename: "bundle.js",
         path: __dirname + "/dist"
     },
@@ -17,10 +20,10 @@ module.exports = {
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
         ]
     },
 
@@ -31,5 +34,37 @@ module.exports = {
     externals: {
         "react": "React",
         "react-dom": "ReactDOM"
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),  // 热更新插件
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: "commons",
+                    chunks: "initial",
+                    minChunks: 2
+                }
+            }
+        }
+    },
+    devServer: {
+        contentBase: "./",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        host: 'localhost',
+        port: 3000,
+        inline: true,//实时刷新
+        hot: true,//Enable webpack's Hot Module Replacement feature
+        compress: true,//Enable gzip compression for everything served
+        overlay: true, //Shows a full-screen overlay in the browser
+        stats: "errors-only",//To show only errors in your bundle
+        open: true, //When open is enabled, the dev server will open the browser.
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                pathRewrite: {"^/api": ""}
+            }
+        }//重定向
     }
 };
